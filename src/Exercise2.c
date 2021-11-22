ce6ade47311aaab4dde6937238bdd82c16e74359
ITITIU20125 <ITITIU20215@student.hcmiu.edu.vn>
2021-11-22 19:23:08 +0700

    Update Ex2

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -14,7 +14,25 @@ Ex:
 
 void Ex2(int n, char *str[]){
 	//Your codes here
-	
+	for(int i = 0; i < n; i++)
+	{
+	   for(int j = i + 1; j < n; j++)
+	   {
+		   char temp [30] ;
+		  if (strcmp(str[i], str[j]) > 0) 
+		   {
+                strcpy(temp, str[i]);
+                strcpy(str[i], str[j]);
+                strcpy(str[j], temp);
+            }
+	    }
+	}
+	for(int i = 0; i < n; i++)
+	{
+	   
+		   printf("%s ", str[i]);
+	   
+	}
 }
 
 int main(int argc, char *argv[]) {
